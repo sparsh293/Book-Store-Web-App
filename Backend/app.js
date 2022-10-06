b6e26bbffa47ba46a0dcdd3cf312f594fd011f8b
Sparsh <sparshnarang293@gmail.com>
2022-10-06 15:24:47 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:QWWARr8DhUqIevJj@cluster0.xnirc42.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
+//QWWARr8DhUqIevJj
